91c1de55646b14247756551ebd9692f6e0fe955f
Miguel Antonio Barcala <ma.barcala16@gmail.com>
2026-01-16 21:32:02 +0800

    Initial commit for Midterm Exam

---
+++ BARCALA_MidtermExam/BARCALA_MidtermExam/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace BARCALA_MidtermExam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtPassengers = new System.Windows.Forms.TextBox();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.txtDateBooked = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbtnEconomy = new System.Windows.Forms.RadioButton();
+            this.rbtnBusiness = new System.Windows.Forms.RadioButton();
+            this.rbtnSingapore = new System.Windows.Forms.RadioButton();
+            this.rbtnThailand = new System.Windows.Forms.RadioButton();
+            this.rbtnHongKong = new System.Windows.Forms.RadioButton();
+            this.rbtnMalaysia = new System.Windows.Forms.RadioButton();
+            this.rbtnSouthKorea = new System.Windows.Forms.RadioButton();
+            this.lblFareDetails = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnDisplayFare = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPassengers);
+            this.groupBox1.Controls.Add(this.txtContact);
+            this.groupBox1.Controls.Add(this.txtDateBooked);
+            this.groupBox1.Controls.Add(this.txtAddress);
+            this.groupBox1.Controls.Add(this.txtFirstName);
+            this.groupBox1.Controls.Add(this.txtLastName);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(29, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 277);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Personal Data";
+            // 
+            // txtPassengers
+            // 
+            this.txtPassengers.Location = new System.Drawing.Point(178, 227);
+            this.txtPassengers.Name = "txtPassengers";
+            this.txtPassengers.Size = new System.Drawing.Size(165, 23);
+            this.txtPassengers.TabIndex = 11;
+            // 
+            // txtContact
+            // 
+            this.txtContact.Location = new System.Drawing.Point(178, 185);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(165, 23);
+            this.txtContact.TabIndex = 10;
+            // 
+            // txtDateBooked
+            // 
+            this.txtDateBooked.Location = new System.Drawing.Point(178, 145);
+            this.txtDateBooked.Name = "txtDateBooked";
+            this.txtDateBooked.Size = new System.Drawing.Size(165, 23);
+            this.txtDateBooked.TabIndex = 9;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(178, 101);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(165, 23);
+            this.txtAddress.TabIndex = 8;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(178, 60);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(165, 23);
+            this.txtFirstName.TabIndex = 7;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(178, 21);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(165, 23);
+            this.txtLastName.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 234);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 16);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "No. of Passengers";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Contact Details";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Date Booked";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last Name";
+            // 
+            // rbtnEconomy
+            // 
+            this.rbtnEconomy.AutoSize = true;
+            this.rbtnEconomy.Location = new System.Drawing.Point(23, 29);
+            this.rbtnEconomy.Name = "rbtnEconomy";
+            this.rbtnEconomy.Size = new System.Drawing.Size(80, 20);
+            this.rbtnEconomy.TabIndex = 2;
+            this.rbtnEconomy.TabStop = true;
+            this.rbtnEconomy.Text = "Economy";
+            this.rbtnEconomy.UseVisualStyleBackColor = true;
+            // 
+            // rbtnBusiness
+            // 
+            this.rbtnBusiness.AutoSize = true;
+            this.rbtnBusiness.Location = new System.Drawing.Point(23, 62);
+            this.rbtnBusiness.Name = "rbtnBusiness";
+            this.rbtnBusiness.Size = new System.Drawing.Size(77, 20);
+            this.rbtnBusiness.TabIndex = 3;
+            this.rbtnBusiness.TabStop = true;
+            this.rbtnBusiness.Text = "Business";
+            this.rbtnBusiness.UseVisualStyleBackColor = true;
+            // 
+            // rbtnSingapore
+            // 
+            this.rbtnSingapore.AutoSize = true;
+            this.rbtnSingapore.Location = new System.Drawing.Point(31, 30);
+            this.rbtnSingapore.Name = "rbtnSingapore";
+            this.rbtnSingapore.Size = new System.Drawing.Size(86, 20);
+            this.rbtnSingapore.TabIndex = 4;
+            this.rbtnSingapore.TabStop = true;
+            this.rbtnSingapore.Text = "Singapore";
+            this.rbtnSingapore.UseVisualStyleBackColor = true;
+            // 
+            // rbtnThailand
+            // 
+            this.rbtnThailand.AutoSize = true;
+            this.rbtnThailand.Location = new System.Drawing.Point(31, 70);
+            this.rbtnThailand.Name = "rbtnThailand";
+            this.rbtnThailand.Size = new System.Drawing.Size(76, 20);
+            this.rbtnThailand.TabIndex = 5;
+            this.rbtnThailand.TabStop = true;
+            this.rbtnThailand.Text = "Thailand";
+            this.rbtnThailand.UseVisualStyleBackColor = true;
+            // 
+            // rbtnHongKong
+            // 
+            this.rbtnHongKong.AutoSize = true;
+            this.rbtnHongKong.Location = new System.Drawing.Point(31, 111);
+            this.rbtnHongKong.Name = "rbtnHongKong";
+            this.rbtnHongKong.Size = new System.Drawing.Size(89, 20);
+            this.rbtnHongKong.TabIndex = 6;
+            this.rbtnHongKong.TabStop = true;
+            this.rbtnHongKong.Text = "Hongkong";
+            this.rbtnHongKong.UseVisualStyleBackColor = true;
+            // 
+            // rbtnMalaysia
+            // 
+            this.rbtnMalaysia.AutoSize = true;
+            this.rbtnMalaysia.Location = new System.Drawing.Point(31, 153);
+            this.rbtnMalaysia.Name = "rbtnMalaysia";
+            this.rbtnMalaysia.Size = new System.Drawing.Size(77, 20);
+            this.rbtnMalaysia.TabIndex = 7;
+            this.rbtnMalaysia.TabStop = true;
+            this.rbtnMalaysia.Text = "Malaysia";
+            this.rbtnMalaysia.UseVisualStyleBackColor = true;
+            // 
+            // rbtnSouthKorea
+            // 
+            this.rbtnSouthKorea.AutoSize = true;
+            this.rbtnSouthKorea.Location = new System.Drawing.Point(31, 195);
+            this.rbtnSouthKorea.Name = "rbtnSouthKorea";
+            this.rbtnSouthKorea.Size = new System.Drawing.Size(99, 20);
+            this.rbtnSouthKorea.TabIndex = 8;
+            this.rbtnSouthKorea.TabStop = true;
+            this.rbtnSouthKorea.Text = "South Korea";
+            this.rbtnSouthKorea.UseVisualStyleBackColor = true;
+            // 
+            // lblFareDetails
+            // 
+            this.lblFareDetails.AutoSize = true;
+            this.lblFareDetails.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFareDetails.Location = new System.Drawing.Point(410, 303);
+            this.lblFareDetails.Name = "lblFareDetails";
+            this.lblFareDetails.Size = new System.Drawing.Size(13, 16);
+            this.lblFareDetails.TabIndex = 9;
+            this.lblFareDetails.Text = "*";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbtnEconomy);
+            this.groupBox2.Controls.Add(this.rbtnBusiness);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(29, 328);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Fare Class Type";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.rbtnSingapore);
+            this.groupBox3.Controls.Add(this.rbtnThailand);
+            this.groupBox3.Controls.Add(this.rbtnHongKong);
+            this.groupBox3.Controls.Add(this.rbtnSouthKorea);
+            this.groupBox3.Controls.Add(this.rbtnMalaysia);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(457, 34);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(171, 242);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Destination (Round Trip)";
+            // 
+            // btnDisplayFare
+            // 
+            this.btnDisplayFare.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplayFare.Location = new System.Drawing.Point(276, 355);
+            this.btnDisplayFare.Name = "btnDisplayFare";
+            this.btnDisplayFare.Size = new System.Drawing.Size(119, 52);
+            this.btnDisplayFare.TabIndex = 12;
+            this.btnDisplayFare.Text = "Display Fare";
+            this.btnDisplayFare.UseVisualStyleBackColor = true;
+            this.btnDisplayFare.Click += new System.EventHandler(this.btnDisplayFare_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(410, 290);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Fare Details";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.BackgroundImage = global::BARCALA_MidtermExam.Properties.Resources.white_texture_background_kolol10tvs4xlfhp;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(658, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnDisplayFare);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.lblFareDetails);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.TextBox txtDateBooked;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtPassengers;
+        private System.Windows.Forms.RadioButton rbtnEconomy;
+        private System.Windows.Forms.RadioButton rbtnBusiness;
+        private System.Windows.Forms.RadioButton rbtnSingapore;
+        private System.Windows.Forms.RadioButton rbtnThailand;
+        private System.Windows.Forms.RadioButton rbtnHongKong;
+        private System.Windows.Forms.RadioButton rbtnMalaysia;
+        private System.Windows.Forms.RadioButton rbtnSouthKorea;
+        private System.Windows.Forms.Label lblFareDetails;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnDisplayFare;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ BARCALA_MidtermExam/BARCALA_MidtermExam/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BARCALA_MidtermExam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplayFare_Click(object sender, EventArgs e)
+        {
+            
+            int numberOfPassengers = int.Parse(txtPassengers.Text);
+
+            
+            int fare = 0;
+            if (rbtnSingapore.Checked) fare = 8000;
+            else if (rbtnThailand.Checked) fare = 7500;
+            else if (rbtnHongKong.Checked) fare = 9000;
+            else if (rbtnMalaysia.Checked) fare = 7700;
+            else if (rbtnSouthKorea.Checked) fare = 9500;
+
+            
+            if (rbtnBusiness.Checked)
+            {
+                fare = (int)(fare * 0.75); 
+            }
+
+            
+            int totalFare = fare * numberOfPassengers;
+
+            
+            lblFareDetails.Text = $"Total Fare for {numberOfPassengers} passenger(s): {totalFare}.00\n" +
+                                  $"Customer Details:\n" +
+                                  $"Name: {txtFirstName.Text} {txtLastName.Text}\n" +
+                                  $"Address: {txtAddress.Text}\n" +
+                                  $"Date Booked: {txtDateBooked.Text}\n" +
+                                  $"Contact Info: {txtContact.Text}\n" +
+                                  $"Destination: {(rbtnSingapore.Checked ? "Singapore" : rbtnThailand.Checked ? "Thailand" : rbtnHongKong.Checked ? "Hong Kong" : rbtnMalaysia.Checked ? "Malaysia" : "South Korea")}";
+        }
+    }
+}
